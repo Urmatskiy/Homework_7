229155e7c65e230abe51b264e116f58f8068f087
Urmatskiy <nickiton14@gmail.com>
2022-12-21 10:14:27 +0300

    Выполнил 52 задание

---
+++ task_52/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] CreateMassive(int rows, int columns, int first, int last)
+{
+    int[,] arr = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            arr[i,j] = new Random().Next(first, last + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindAverageInRow(int[,] arr)
+{
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    double result = 0;
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            result += arr[j, i];
+        }
+        Console.Write($"{result / arr.GetLength(0)}; ");
+        result = 0;
+    }
+}
+
+int rows = EnterData("Введите количество строк: ");
+int columns = EnterData("Введите количество столбцов: ");
+int[,] array = CreateMassive(rows, columns, 0, 9);
+PrintArray(array);
+Console.WriteLine();
+FindAverageInRow(array);
